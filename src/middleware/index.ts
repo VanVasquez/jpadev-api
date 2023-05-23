c673f818df40e47fb14dd1dfe9b373a5e9cf03d5
Van Vincent Vasquez <vanvasquez0725@gmail.com>
2023-05-23 12:49:33 +0800

    new origin

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,6 +3,8 @@ import { NextFunction, Request, Response } from "express";
 //  Allowed origins 
 export const allowedOrigins = [
   "http://localhost:3000",
+  "jpadev-client-e4o7hk530-vanvasquez.vercel.app",
+  "jpadev-client.vercel.app"
 ];
 
 // Credentials
